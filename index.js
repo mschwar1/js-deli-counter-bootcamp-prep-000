09567dec922a33d5c2bbe226bef553d8658fa8fb
Mai Schwartz <mai.schwartz@gmail.com>
2018-11-17 17:53:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,16 +16,15 @@ function nowServing(deliLine) {
 
 function currentLine(deliLine) {
   while (deliLine.length >= 0)
-  if (deliLine.length === 0) {
+      if (deliLine.length === 0) {
     return "The line is currently empty."
-  }
-  else {
-    var lineCount = 0
-    announcement = []
-    while (lineCount < deliLine.length) {
-      announcement.push(`${lineCount+1}. ${deliLine[lineCount]}`);
-      lineCount++
-    }
-      return `The line is currently: ${announcement},`
+      } else {
+          var lineCount = 0
+          announcement = []
+          while (lineCount < deliLine.length) {
+            announcement.push(`${lineCount+1}. ${deliLine[lineCount]}`);
+            lineCount++
+          }
+      return `The line is currently: ${announcement}`
   }
 }
